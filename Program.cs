c621f83e7bcec263bb231038157c4bb7ee7106b0
Alex 'Ript' Malyshev <alexript@gmail.com>
2020-02-01 02:02:08 +0300

    using ini-parser

--- Program.cs
+++ Program.cs
@@ -3,14 +3,17 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Logging.Serilog;
-
+using mdNodeConfig;
 
 namespace mdNote
 {
     class Program
     {
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            Config cfg = new Config();
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
---
+++ src/config/Config.cs
@@ -0,0 +1,14 @@
+using IniParser;
+using IniParser.Model;
+
+namespace mdNodeConfig
+{
+    class Config
+    {
+        public Config()
+        {
+            var parser = new FileIniDataParser();
+            IniData data = parser.ReadFile(".mdNode.ini");
+        }
+    }
+}
