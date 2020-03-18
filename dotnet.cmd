30d3fd287f1393bd6ac1a0b326ffb499251df91d
Alex 'Ript' Malyshev <alexript@gmail.com>
2020-03-18 17:17:45 +0300

    update sdk version

--- dotnet.cmd
+++ dotnet.cmd
@@ -1,7 +1,8 @@
 @echo off
+set _DOTNET_SDK_VERSION=3.1.200
 if defined DOTNETCORE_PATH ( 
     %DOTNETCORE_PATH%\dotnet.exe %*
 ) else (
-    ..\..\DotNET\SDK\3.1.101\dotnet.exe %*
+    ..\..\DotNET\SDK\%_DOTNET_SDK_VERSION%\dotnet.exe %*
 )
 
